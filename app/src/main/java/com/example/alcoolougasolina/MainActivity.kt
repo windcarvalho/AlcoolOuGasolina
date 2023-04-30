0f6107a45e7ba2df3260735d1444487e8bd0a22e
Victor <adm.victorfernando@gmail.com>
2023-04-30 13:58:30 -0300

    Update MainActivity.kt

--- app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
+++ app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
@@ -8,11 +8,30 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     var percentual:Double = 0.7
+    var edGasolina:Double = 0.0;
+    var edAlcool:Double = 0.0;
+    var resDouble:Double = 0.0;
+    resCompleto df = new DecimalFormat("#.00");
+    resultadoVal=df.format(0.0000);
+    
     Switch swPercentual;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("PDM23","No onCreate, $percentual")
+        
+        TextView resultado = (TextView) findViewById(R.id.resultado);
+        resultado.setText("$resultadoVal");
+        //texto com resultado
+        
+        EditText edGasolina = (EditText) findViewById(R.id.edGasolina);
+        EditText edAlcool = (EditText) findViewById(R.id.edAlcool);
+        String txtGasolina = edGasolina.getText().toString();
+        String txtAlcool = edAlcool.getText().toString();
+        edGasolina = Double.parseDouble(txtGasolina);
+        edAlcool = Double.parseDouble(txtAlcool);
+        //recuperando textos dos imputs e convertendo para double
+        
         Switch swPercentual = (Switch) findViewById(R.id.swPercentual);
         /*O switch deve ser adicionado sempre após a set content view
           para poder ser visualizado corretamente.
@@ -31,14 +50,24 @@ class MainActivity : AppCompatActivity() {
         btCalc.setOnClickListener(View.OnClickListener {
             //código do evento
             if (swPercentual.isChecked()){
-                percentual=0.75
-                Log.d("PDM23","No btCalcular, $percentual")
-                statusSwitch1 = simpleSwitch1.getTextOn().toString();
+                percentual=0.75;
+                resDouble = edGasolina/edAlcool;
+                resultadoVal=df.format(resDouble);
+                resultado.setText("$resultadoVal");
+                Log.d("PDM23","No btCalcular, $percentual");
+                Log.d("PDM23","No btCalcular, $resultadoVal");
                 }
             else{
-                
+                percentual=0.70;
+                resDouble = edGasolina/edAlcool;
+                resultadoVal=df.format(resDouble);
+                resultado.setText("$resultadoVal");
+                Log.d("PDM23","No btCalcular, $percentual");
+                Log.d("PDM23","No btCalcular, $resultadoVal");
             }
         })
+        //termino do botão
+        
     }
 override fun onResume(){
     super.onResume()
