ce417ffa431d351c26dce8b5aaf9b20dcc3c2004
Victor <adm.victorfernando@gmail.com>
2023-04-30 13:22:24 -0300

    Update MainActivity.kt

--- app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
+++ app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
@@ -8,16 +8,36 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     var percentual:Double = 0.7
+    Switch swPercentual;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("PDM23","No onCreate, $percentual")
-
+        Switch swPercentual = (Switch) findViewById(R.id.swPercentual);
+        /*O switch deve ser adicionado sempre após a set content view
+          para poder ser visualizado corretamente.
+          Lembrando que a funcao .isChecked() verifica se está ligado ou não
+          */
+        swPercentual.setTextOn("75%");
+        swPercentual.setTextOff("70%");
+        //textos para on e off
+        
+        swPercentual.getChecked(true);
+        //setando ele como ligado
+        swPercentual.getChecked(false);
+        //setando ele como desligado
+        
         val btCalc: Button = findViewById(R.id.btCalcular)
         btCalc.setOnClickListener(View.OnClickListener {
             //código do evento
-            percentual=0.75
-            Log.d("PDM23","No btCalcular, $percentual")
+            if (swPercentual.isChecked()){
+                percentual=0.75
+                Log.d("PDM23","No btCalcular, $percentual")
+                statusSwitch1 = simpleSwitch1.getTextOn().toString();
+                }
+            else{
+                
+            }
         })
     }
 override fun onResume(){
